0bc8d7434cb4281adf47a57e329fb8d438bdb50d
Ailsa Meechan-Maddon <ailsamm@outlook.com>
2020-06-16 06:34:33 -0700

    Initial commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,15 @@
 const app = require('./app');
+const knex = require('knex');
+const { PORT, DATABASE_URL } = require('./config');
 
-const { PORT } = require('./config');
+// Initialise Knex instance
+const db = knex({
+    client: 'pg',
+    connection: DATABASE_URL,
+});
+  
+// Allow 'db' to be accessed by dependents
+app.set('db', db);
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
